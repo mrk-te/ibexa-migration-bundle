9ed4ea3d37b95493e79c7cbd8518272f590b3e77
Marc Teinturier <marc.teinturier@gmail.com>
2025-09-15 19:08:16 +0200

    feat[Ibexa 5.0] Fix assertRegExp and assertNotRegExp deprecation

--- Tests/phpunit/03_GenerateTest.php
+++ Tests/phpunit/03_GenerateTest.php
@@ -136,7 +136,7 @@ class GenerateTest extends CommandExecutingTest
     {
         // Check that the generated file can be parsed as valid Migration Definition
         $output = $this->runCommand('kaliop:migration:status');
-        $this->assertRegExp('?\| ' . basename($filePath) . ' +\| not executed +\|?', $output);
+        $this->assertMatchesRegularExpression('?\| ' . basename($filePath) . ' +\| not executed +\|?', $output);
 
         // We should really test generated migrations by executing them, but for the moment we have a few problems:
         // 1. we should patch them after generation, eg. replacing 'folder' w. something else (to be able to create and delete the content-type)
@@ -144,7 +144,7 @@ class GenerateTest extends CommandExecutingTest
         // 3. generated migration for 'folder' contenttype has a borked field-settings definition
         if (false) {
             $output = $this->runCommand('kaliop:migration:migrate', array('--path' => array($filePath), '-n' => null));
-            $this->assertRegexp('?\| ' . basename($filePath) . ' +\| +\|?', $output);
+            $this->assertMatchesRegularExpression('?\| ' . basename($filePath) . ' +\| +\|?', $output);
         }
     }
 }
